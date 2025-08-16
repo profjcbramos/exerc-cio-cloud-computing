bf6a64e913b129cda9e20333168d51736ec2b4b8
profjcbramos <prof.jcbarcelos@gmail.com>
2025-08-16 22:03:48 +0000

    Atualiza título no app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(page_title="Meu primeiro app", layout="wide")
+st.title("Olá, Streamlit no GitHub! 🎈")
+st.write("Se você está lendo isto, funcionou! 😄")
